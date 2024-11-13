ffd343c7a3eb9a64d53bdb662cf6d64606abcf80
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-13 20:56:06 +0530

    added images and setting up auth page

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className="bg-brand p-10">
+        <div>
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto"
+          />
+
+          <div className="space-y-5 text-white">
+            <h1 className="h1">Manage your files the best way</h1>
+            <p className="body-1">
+              This is a place where you can store all your documents
+            </p>
+          </div>
+        </div>
+      </section>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
