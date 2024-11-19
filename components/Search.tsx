972d9ebe461c460b6d2e0c84d7c8f2cff60f0f62
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-19 17:27:32 +0530

    added styles to search

--- components/Search.tsx
+++ components/Search.tsx
@@ -6,6 +6,8 @@ import { Input } from "./ui/input";
 import { useSearchParams } from "next/navigation";
 import { getFiles } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
+import Thumbnail from "./Thumbnail";
+import FormattedDateTime from "./FormattedDateTime";
 
 const Search = () => {
   const [query, setQuery] = useState("");
@@ -52,7 +54,30 @@ const Search = () => {
         {open && (
           <ul className="search-result">
             {results.length > 0 ? (
-              results.map((file) => <li key={file.$id}>{file.name}</li>)
+              results.map((file) => (
+                <li
+                  key={file.$id}
+                  className="flex items-center juber
+              "
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption text-light-200 line-clamp-1"
+                  />
+                </li>
+              ))
             ) : (
               <p className="empty-result">No files found</p>
             )}
