5240562ed20c8508b5e3d0b5ab8cca1038a0ea45
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-19 17:53:42 +0530

    implemented debounce to search

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,44 +1,40 @@
 "use client";
 
-import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import { Input } from "./ui/input";
+
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { getFiles } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
-import Thumbnail from "./Thumbnail";
-import FormattedDateTime from "./FormattedDateTime";
-
+import Thumbnail from "@/components/Thumbnail";
+import FormattedDateTime from "@/components/FormattedDateTime";
+import { useDebounce } from "use-debounce";
 const Search = () => {
   const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
   const [results, setResults] = useState<Models.Document[]>([]);
   const [open, setOpen] = useState(false);
-
   const router = useRouter();
-
-  const searchParams = useSearchParams();
-
   const path = usePathname();
-
-  const searchQuery = searchParams.get("query") || "";
+  const [debouncedQuery] = useDebounce(query, 300);
 
   useEffect(() => {
     const fetchFiles = async () => {
-      if (!query) {
+      if (debouncedQuery.length === 0) {
         setResults([]);
         setOpen(false);
-
         return router.push(path.replace(searchParams.toString(), ""));
       }
 
-      const files = await getFiles({ searchText: query });
-
+      const files = await getFiles({ types: [], searchText: debouncedQuery });
       setResults(files.documents);
       setOpen(true);
     };
 
     fetchFiles();
-  }, [query]);
+  }, [debouncedQuery]);
 
   useEffect(() => {
     if (!searchQuery) {
@@ -60,7 +56,7 @@ const Search = () => {
       <div className="search-input-wrapper">
         <Image
           src="/assets/icons/search.svg"
-          alt="search"
+          alt="Search"
           width={24}
           height={24}
         />
@@ -76,8 +72,8 @@ const Search = () => {
             {results.length > 0 ? (
               results.map((file) => (
                 <li
-                  key={file.$id}
                   className="flex items-center justify-between"
+                  key={file.$id}
                   onClick={() => handleClickItem(file)}
                 >
                   <div className="flex cursor-pointer items-center gap-4">
@@ -94,7 +90,7 @@ const Search = () => {
 
                   <FormattedDateTime
                     date={file.$createdAt}
-                    className="caption text-light-200 line-clamp-1"
+                    className="caption line-clamp-1 text-light-200"
                   />
                 </li>
               ))
